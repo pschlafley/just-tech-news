967ebbb4d1f767299f4d8f0257af02ea77eb13c3
Peyton Schlafley <pschlafley0@gmail.com>
2020-10-29 11:35:16 -0400

    create comments api route to add comments to posts

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const Post = require('./Post');
 
 const Vote = require('./Vote');
 
+const Comment = require('./Comment');
+
 // create associations 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -41,4 +43,20 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Comment.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,8 +4,12 @@ const userRoutes = require('./user-routes.js');
 
 const postRoutes = require('./post-routes.js');
 
+const commentRoutes = require('./comments-routes.js');
+
 router.use('/users', userRoutes);
 
-router.use('/posts',postRoutes);
+router.use('/posts', postRoutes);
+
+router.use('/comments', commentRoutes);
 
 module.exports = router;
