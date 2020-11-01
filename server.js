eb563049de3c69239b94c7cde1b8dce0b4b0d24f
Peyton Schlafley <pschlafley0@gmail.com>
2020-11-01 16:15:58 -0500

    added login/logout functionality

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const path = require('path');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true, 
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
 // html template engine
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -13,6 +28,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
